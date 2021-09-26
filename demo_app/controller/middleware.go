19c130e3ed51ac16743c7da65bf1b99c6eec5506
Hien Nguyen <mr.nguyenhien1994@gmail.com>
2021-09-26 19:48:37 +0700

    Tidy go modules

--- demo_app/controller/middleware.go
+++ demo_app/controller/middleware.go
@@ -1,19 +1,22 @@
 package controller
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
 func AuthenHandler() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// TODO: parse jwts and set to context
+		log.Println(c.Request.Header)
 		c.Next()
 	}
 }
 
 // Authorize determines if current subject has been authorized to take an action on an object.
 func Authorize() gin.HandlerFunc {
-	return func(c *gin.Context) {
+	return func(c *gin.Context	) {
 		// TODO: additional check
 		c.Next()
 	}
